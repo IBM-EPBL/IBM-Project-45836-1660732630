1afb3280fc8126e2e39990bf401587a787ebc54f
Raagaaa <112246023+Raagaaa@users.noreply.github.com>
2022-11-18 22:18:29 +0530

    Update sendgridmail.py

--- Final Deliverables/Codind and Solutioning/sendgridmail.py
+++ Final Deliverables/Codind and Solutioning/sendgridmail.py
@@ -3,8 +3,8 @@ import os
 from sendgrid.helpers.mail import *
 api_key = "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"
 sg = sendgrid.SendGridAPIClient(api_key)
-from_email = Email("sabanaashmi22022002@gmail.com")
-to_email = To("dsraga7@gmail.com")
+from_email = Email("sabana_shmi22022002@gmail.com")
+to_email = To("ds_aga7@gmail.com")
 subject = "Your little efforts can give others second chances to live life."
 content = Content("text/plain", "Thank you for choosing our plasma donor application for donating plasma. Your account has been created and one step ahead to go, please verify your email ID.")
 mail = Mail(from_email, to_email, subject, content)
